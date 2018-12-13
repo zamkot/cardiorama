18e8c4d7bbde99cdd165d25e60f8ad0922f9e926
zamkot <zamkot.z@gmail.com>
2018-12-13 20:33:48 +0100

    Revert "RPeaks module added."

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
-#include <RPeaksModule.h>
 
 int main() {
     std::cout << "To jest cardiorama. Jeszcze nic tu nie ma" << std::endl;
-
-    // test modułu rpeaks
-//    Physionet ioModule("test");
-//    RPeaksModule rPeaksModule(ioModule);
-//    rPeaksModule.configure("resources/100.dat", HILBERT);
-//    auto results = rPeaksModule.getResults();
-//    std::cout << "RPEAKS" << results.rpeaks.at(0) << std::endl << std::flush;
-
 }
