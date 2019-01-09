d7229b9cd4e6d93d01e8cc71dc12b75e27fe4aa8
fncvsig <45051422+fncvsig@users.noreply.github.com>
2019-01-10 00:19:28 +0100

    Update Datatypes.hpp

--- include/Datatypes.hpp
+++ include/Datatypes.hpp
@@ -18,7 +18,13 @@ struct RPeaksData {
 struct WavesData {
     std::vector<int> qrsOnset;
     std::vector<int> qrsEnd;
+    std::vector<int> tOnset;
     std::vector<int> tEnd;
     std::vector<int> pOnset;
     std::vector<int> pEnd;
+};
+
+struct TWAData {
+	double maxTWA
+    std::vector<int> resultTWA;
 };
