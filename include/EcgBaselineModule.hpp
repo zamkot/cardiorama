ffcbc8381b3081375fcec4c663c6891057b6556d
tomekzamkotowicz <zamkot.z@gmail.com>
2019-01-21 13:38:27 +0100

    checkout EcgBaselineModule from ecg-baseline-module2

--- include/EcgBaselineModule.hpp
+++ include/EcgBaselineModule.hpp
@@ -1,25 +1,25 @@
 #pragma once
-#include <ModuleBase.hpp>
-#include <Datatypes.hpp>
 #include <EcgBaselineModuleBase.hpp>
 
 class EcgBaselineModule : public EcgBaselineModuleBase {
 
     EcgBaselineData results; 
     EcgBaselineConfig config; 
+    bool isConfigured = false;
 
     void runEcgBaseline(); 
 
-    std::vector<double> filt (const std::vector<double>& sos, EcgBaselineData signal);
-    std::vector<double> filtfilt (const std::vector<double>& sos, EcgBaselineData signal);
-    std::vector<double> processButter(EcgBaselineData signal);
-    std::vector<double> processWavelet(EcgBaselineData signal);
+    std::vector<double> filt( std::vector<double> &sos,  std::vector<double> &signal);
+    std::vector<double> filtfilt( std::vector<double> &sos,  std::vector<double> &signal);
+    std::vector<double> processButter( std::vector<double> &signal);
+    std::vector<double> processWavelet(std::vector<double> &signal);
+
 
 public:
 
     EcgBaselineData getResults() override;
-    void configure(EcgBaselineConfig) override; 
+    void configure(EcgBaselineConfig::Algorithm) override;
     
-    void invalidateResults() override;
+  //  void invalidateResults() override;
 
 };
--- include/EcgBaselineModuleBase.hpp
+++ include/EcgBaselineModuleBase.hpp
@@ -1,18 +1,20 @@
 #pragma once
 #include <ModuleBase.hpp>
 
-
 struct EcgBaselineData {
     std::vector<double> samples;
 };
 
+
 struct EcgBaselineConfig {
-    enum Algorithm { BUTTERWORTH, WAVELET };
-    Algorithm algorithm;
+   enum Algorithm { 
+       BUTTERWORTH, WAVELET 
+       };
+   Algorithm algorithm;
 };
 
 class EcgBaselineModuleBase : public ModuleBase {
 public:
     virtual EcgBaselineData getResults() = 0;
-    virtual void configure(EcgBaselineConfig) = 0; 
-};
+    virtual void configure(EcgBaselineConfig::Algorithm) = 0; 
+}; 
