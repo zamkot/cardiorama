8fdf78a00bf13ebb248d2903f660112e91b0b95e
fncvsig <wydrawmydle@wp.pl>
2019-01-23 02:05:30 +0100

    TWaves example

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
-#include<TWavesModule.hpp>
-#include<DataFrame.hpp>
-#include <Datatypes.hpp>
+#include <iostream>
+#include <vector>
+#include <RPeaksModuleStub.hpp>
+#include <HrvDfaModule.hpp>
+#include <DataFrame.hpp>
 
+int main(int argc, char** argv) {
+    using namespace std;
 
-int main() {
+    if (argc != 2) {
+        cout << "Invalid number of arguments" << endl;
+        return 1;
+    }
 
-std::cout << "WELCOME TRAVELLER 1 \n";
+    string path = argv[1];
+    DataFrame df_in(path);
+    
+    vector<int> rpeaks = df_in.get<int>("Rpeaks", [](string s){ return std::stoi(s); });
+    
+    RPeaksModuleStub rPeaksModule(rpeaks);
+    HrvDfaModule hrvDfaModule(rPeaksModule);
 
-TWavesModule::runTWaves();
+    HrvDfaData hrvDfaData = hrvDfaModule.getResults();
+    
+    DataFrame df_out;
 
-std::cout << "WELCOME TRAVELLER 2 \n";
+    df_out.addColumn("log_window_sizes", hrvDfaData.log_window_sizes);
+    df_out.addColumn("log_fluctuation", hrvDfaData.log_fluctuation);
+    df_out.addColumn("line_alfa1", hrvDfaData.line_alfa1);
+    df_out.addColumn("line_alfa2", hrvDfaData.line_alfa2);
 
-
-   
+    df_out.toCsv("DFA_output.csv");
 }
